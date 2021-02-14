6b2312412b0836efc409c1730ca5f65968e52dbb
ViLSa <vishnuagbly@gmail.com>
2021-02-15 00:14:16 +0530

    faster marking of possible moves.

---
+++ lib/components/piece.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class Piece extends StatefulWidget {
+  Piece(this.cellWidth, {this.initState, this.initValue = -1, this.flip});
+
+  final Function(PieceState state) initState;
+  final Function flip;
+  final double cellWidth;
+  final int initValue;
+
+  @override
+  PieceState createState() => PieceState(initValue);
+}
+
+class PieceState extends State<Piece> {
+  PieceState(int value) {
+    if (value == 1) this._value = 2;
+    else this._value = value;
+  }
+
+  int _value;
+  bool possibleMove = false;
+  int get boardValue {
+    if (_value == 0 || _value == 3) return 0;
+    if (_value == 1 || _value == 2) return 1;
+    return -1;
+  }
+  int get value => _value;
+
+  static bool whiteTurn = false;
+
+  @override
+  void initState() {
+    widget.initState(this);
+    super.initState();
+  }
+
+  void stateFn({bool operate = true}) {
+    setState(() {
+      if (operate) _value = (_value + 1) % 4;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget child = Container();
+    if (possibleMove)
+      child = Center(
+        child: Container(
+          width: widget.cellWidth / 2,
+          height: widget.cellWidth / 2,
+          decoration: BoxDecoration(
+            color: whiteTurn ? Colors.white54 : Colors.black54,
+            borderRadius: BorderRadius.circular(50),
+          ),
+        ),
+      );
+    if (_value == 0)
+      child = FittedBox(
+        fit: BoxFit.cover,
+        child: Image.asset("assets/flip_0/frame_0.png"),
+      );
+    else if (_value == 2)
+      child = FittedBox(
+        fit: BoxFit.cover,
+        child: Image.asset("assets/flip_0/frame_18.png"),
+      );
+    return Container(
+      padding: EdgeInsets.all(1),
+      width: widget.cellWidth,
+      height: widget.cellWidth,
+      color: Colors.black,
+      child: InkWell(
+        onTap: () {
+          if (_value == -1 && possibleMove) {
+            if (!whiteTurn) _value = 1;
+            whiteTurn = !whiteTurn;
+            stateFn();
+            (widget.flip ?? () {})();
+          }
+        },
+        child: Container(
+          color: Colors.green[600],
+          child: child,
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:image_sequence_animator/image_sequence_animator.dart';
+import 'package:othello/components/piece.dart';
 
 class HomePage extends StatefulWidget {
   HomePage(this.screenWidth);
@@ -19,96 +20,37 @@ class _HomePageState extends State<HomePage>
   double cellWidth;
   List<Widget> mainStack;
   List<List<int>> board = [];
-  List<List<bool>> flipping = [], possibleMove = [];
-  List<List<Function({bool operate})>> cellStateFns = [], flipStateFns = [];
-  bool whiteTurn = true;
+  List<List<bool>> flipping = [];
+  List<List<Function({bool operate})>> flipStateFns = [];
+  List<List<PieceState>> pieceStates = [];
 
   @override
   void initState() {
     boardWidth = widget.screenWidth - 50;
     cellWidth = boardWidth / boardLength;
-    cellStateFns.length = boardHeight;
     flipStateFns.length = boardHeight;
     flipping.length = boardHeight;
-    possibleMove.length = boardHeight;
+    pieceStates.length = boardHeight;
     for (int i = 0; i < boardHeight; i++) {
-      cellStateFns[i] = [];
-      cellStateFns[i].length = boardLength;
       flipStateFns[i] = [];
       flipStateFns[i].length = boardLength;
       flipping[i] = [];
       flipping[i].length = boardLength;
-      possibleMove[i] = [];
+      pieceStates[i] = [];
+      pieceStates[i].length = boardLength;
       board.add([]);
-      for (int j = 0; j < boardLength; j++) {
-        board[i].add(-1);
-        possibleMove[i].add(false);
-      }
+      for (int j = 0; j < boardLength; j++) board[i].add(-1);
     }
 
     initBoard();
     super.initState();
   }
 
-  Widget piece(int i, int j) => StatefulBuilder(
-        builder: (context, setCellState) {
-          Widget child = Container();
-          if (possibleMove[i][j])
-            child = Center(
-              child: Container(
-                width: cellWidth / 2,
-                height: cellWidth / 2,
-                decoration: BoxDecoration(
-                  color: whiteTurn ? Colors.white54 : Colors.black54,
-                  borderRadius: BorderRadius.circular(50),
-                ),
-              ),
-            );
-          if (board[i][j] == 0)
-            child = FittedBox(
-              fit: BoxFit.cover,
-              child: Image.asset("assets/flip_0/frame_0.png"),
-            );
-          else if (board[i][j] == 2)
-            child = FittedBox(
-              fit: BoxFit.cover,
-              child: Image.asset("assets/flip_0/frame_18.png"),
-            );
-          cellStateFns[i][j] = ({bool operate = true}) {
-            setCellState(() {
-              if (operate) board[i][j] = (board[i][j] + 1) % 4;
-            });
-          };
-          return Container(
-            padding: EdgeInsets.all(1),
-            width: cellWidth,
-            height: cellWidth,
-            color: Colors.black,
-            child: InkWell(
-              onTap: () async {
-                if (board[i][j] == -1 && possibleMove[i][j]) {
-                  if (!whiteTurn) board[i][j] = 1;
-                  whiteTurn = !whiteTurn;
-                  cellStateFns[i][j]();
-                  await _flipPieces(_getPiecesToFlip(i, j, board[i][j]));
-                  _markPossibleMoves();
-                  return;
-                }
-              },
-              child: Container(
-                color: Colors.green[600],
-                child: child,
-              ),
-            ),
-          );
-        },
-      );
-
   void initBoard() {
     board[3][3] = 0;
     board[4][4] = 0;
-    board[3][4] = 2;
-    board[4][3] = 2;
+    board[3][4] = 1;
+    board[4][3] = 1;
 
     WidgetsBinding.instance.addPostFrameCallback((_) => _markPossibleMoves());
   }
@@ -120,7 +62,21 @@ class _HomePageState extends State<HomePage>
         children: List.generate(
             boardHeight,
             (i) => Row(
-                  children: List.generate(boardLength, (j) => piece(i, j)),
+                  children: List.generate(
+                      boardLength,
+                      (j) => Piece(
+                            cellWidth,
+                            initValue: board[i][j],
+                            initState: (state) => pieceStates[i][j] = state,
+                            flip: () async {
+                              board[i][j] = pieceStates[i][j].boardValue;
+                              var piecesToFlip =
+                                  _getPiecesToFlip(i, j, board[i][j]);
+                              _flipPieces(piecesToFlip);
+                              _markPossibleMoves();
+                              await _startFlipAnimation(piecesToFlip);
+                            },
+                          )),
                 )),
       ),
     ];
@@ -152,7 +108,7 @@ class _HomePageState extends State<HomePage>
 
   void _flip(int i, int j) {
     flipStateFns[i][j]();
-    cellStateFns[i][j]();
+    pieceStates[i][j].stateFn();
   }
 
   Widget _flipPiece(int i, int j) {
@@ -174,10 +130,10 @@ class _HomePageState extends State<HomePage>
 
   Widget _flipAnimation(int i, int j) {
     final _onFinishPlaying = (state) {
-      if (board[i][j] % 2 == 0) return;
+      if (pieceStates[i][j].value % 2 == 0) return;
       WidgetsBinding.instance.addPostFrameCallback((_) {
         flipStateFns[i][j]();
-        cellStateFns[i][j]();
+        pieceStates[i][j].stateFn();
       });
     };
     return Container(
@@ -202,26 +158,35 @@ class _HomePageState extends State<HomePage>
   }
 
   void _markPossibleMoves() {
-    int value = whiteTurn ? 0 : 2;
+    int value = PieceState.whiteTurn ? 0 : 1;
     for (int i = 0; i < boardHeight; i++)
       for (int j = 0; j < boardLength; j++) {
         if (board[i][j] != -1) continue;
         bool setThisCellState = false;
-        if (possibleMove[i][j]) {
-          possibleMove[i][j] = false;
+        if (pieceStates[i][j].possibleMove) {
+          pieceStates[i][j].possibleMove = false;
           setThisCellState = true;
         }
         if (_getPiecesToFlip(i, j, value).length > 0) {
-          possibleMove[i][j] = true;
+          pieceStates[i][j].possibleMove = true;
           setThisCellState = true;
         }
-        if (setThisCellState) cellStateFns[i][j](operate: false);
+        if (setThisCellState) pieceStates[i][j].stateFn(operate: false);
       }
   }
 
-  Future<void> _flipPieces(List<List<List<int>>> piecesToFlip) async {
+  void _flipPieces(List<List<List<int>>> piecesToFlip) {
+    for (var levelPieces in piecesToFlip)
+      for (var pair in levelPieces) {
+        int i = pair.first, j = pair.last;
+        board[i][j] = 1 - board[i][j];
+      }
+  }
+
+  Future<void> _startFlipAnimation(List<List<List<int>>> piecesToFlip) async {
     for (var levelPieces in piecesToFlip) {
       for (var pair in levelPieces) _flip(pair.first, pair.last);
+
       await Future.delayed(Duration(milliseconds: 100));
     }
     await Future.delayed(Duration(milliseconds: 400));
@@ -248,7 +213,7 @@ class _HomePageState extends State<HomePage>
               currentI >= boardHeight ||
               currentJ < 0 ||
               currentJ >= boardLength ||
-              board[currentI][currentJ] % 2 == 1) {
+              board[currentI][currentJ] == -1) {
             step = -1;
             continue;
           }
